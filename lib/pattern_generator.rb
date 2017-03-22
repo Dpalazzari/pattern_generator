6f608cde1ce1f3ea625a06ffd081574a7c65a405
Drew Palazzari <drewpalazzari@hotmail.com>
2017-03-22 13:49:23 -0600

    Test initialize features
    Fully test the hash the program initializes with.

--- lib/pattern_generator.rb
+++ lib/pattern_generator.rb
@@ -1,6 +1,19 @@
+require 'pry-state'
 class PatternGenerator
 
+  attr_reader :pattern_hash
+
+  def initialize
+    letter_array = %w(A B C D E F G H I J K L M N O P Q R S T U V W X Y Z)
+    number_array = %w(0 1 2 3 4 5 6 7 8 9)
+    @pattern_hash = {
+      '.': letter_array,
+      '#': number_array
+    }
+  end
+
   def verify(input, pattern)
+    binding.pry
     # return true if input matches pattern
     # return false if input !matches pattern
   end
--- test/pattern_generator_test.rb
+++ test/pattern_generator_test.rb
@@ -1,3 +1,4 @@
+require 'pry-state'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/pattern_generator'
@@ -7,6 +8,28 @@ class PatternGeneratorTest < Minitest::Test
   def test_it_exists
     generator = PatternGenerator.new
     assert generator
+  end
+
+  def test_it_initializes_with_a_hash
+    generator = PatternGenerator.new
+    assert_equal Hash, generator.pattern_hash.class
+  end
+
+  def test_initialized_hash_has_expected_values
+    generator = PatternGenerator.new
+    value = generator.pattern_hash[:'.']
+    num_value = generator.pattern_hash[:'#']
+    assert_equal Array, value.class
+    assert_equal 'A', value[0]
+    assert_equal Array, num_value.class
+    assert_equal '0', num_value[0]
+  end
+
+  def test_it_verifies_one_character
+    skip
+    generator = PatternGenerator.new
+    g = generator.verify('A', '.')
+    assert_equal true, g
   end
 
 end
